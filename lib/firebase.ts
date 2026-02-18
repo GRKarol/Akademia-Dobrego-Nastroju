80b2170ad2e57381cba0945682a9a0fecc7ff065
GRKarol <karol.grankers@gmail.com>
2026-02-18 15:10:22 +0100

    Update firebase.ts

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.VITE_FIREBASE_API_KEY || "AIzaSyCw5Wdabyv6KRfGwv4CJ7SMPyvfg-y0dpY",
-  authDomain: "akademia-dobrego-nastroju.firebaseapp.com",
-  projectId: "akademia-dobrego-nastroju",
-  storageBucket: "akademia-dobrego-nastroju.firebasestorage.app",
-  messagingSenderId: "625717319115",
-  appId: "1:625717319115:web:77c009c5bc6e2a8496924e",
-  measurementId: "G-HG8CGYQ301"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
